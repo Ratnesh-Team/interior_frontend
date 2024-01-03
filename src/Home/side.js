a2de6762d4703afb7cac5556886683bf1d4fe272
Arvind Maurya <ar789maurya@gmail.com>
2024-01-03 16:40:29 +0530

    chat integration

--- src/Home/side.js
+++ src/Home/side.js
@@ -8,16 +8,16 @@ const Side = () => {
     <Sidebar >
     
     <Link to='/'><SidebarItem  text="Dashboard"  active={true} alert ></SidebarItem></Link>
-    <Link to='/' ><SidebarItem  text="All Projects" active={true} alert ></SidebarItem></Link>
+    <Link to='/project' ><SidebarItem  text="All Projects" active={true} alert ></SidebarItem></Link>
     <Link to='/quotation'><SidebarItem  text="Quotation" active={true} alert ></SidebarItem></Link>
     <Link to='/file'><SidebarItem  text="Files" active={true} alert ></SidebarItem></Link>
-    <Link to='/'><SidebarItem  text="Timelines" active={true} alert ></SidebarItem></Link>
-    <Link to='/'><SidebarItem  text="Task Manager" active={true} alert ></SidebarItem></Link>
-    <Link to='/'><SidebarItem  text="MOM" active={true} alert ></SidebarItem></Link>
-    <Link to='/'><SidebarItem  text="Moodboard" active={true} alert ></SidebarItem></Link>
-    <Link to='/'><SidebarItem  text="Vender & PO's" active={true} alert ></SidebarItem></Link>
-    <Link to='/'><SidebarItem  text="Chat" active={true} alert ></SidebarItem></Link>
-     <Link to='/'><SidebarItem  text="Reports" active={true} alert ></SidebarItem></Link> 
+    <Link to='/timelines'><SidebarItem  text="Timelines" active={true} alert ></SidebarItem></Link>
+    <Link to='/task'><SidebarItem  text="Task Manager" active={true} alert ></SidebarItem></Link>
+    <Link to='/mom'><SidebarItem  text="MOM" active={true} alert ></SidebarItem></Link>
+    <Link to='/moodboard'><SidebarItem  text="Moodboard" active={true} alert ></SidebarItem></Link>
+    <Link to='/vendor'><SidebarItem  text="Vender & PO's" active={true} alert ></SidebarItem></Link>
+    <Link to='http://localhost:5173/'><SidebarItem  text="Chat" active={true} alert ></SidebarItem></Link>
+     <Link to='/reports'><SidebarItem  text="Reports" active={true} alert ></SidebarItem></Link> 
 
     </Sidebar>
   )
